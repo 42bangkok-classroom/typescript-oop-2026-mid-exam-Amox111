eb3a44d48afa8c45d3c74c15160a8c36e46b849b
Chomchanok khwammana <ruxkwarmmana233@gmail.com>
2026-01-21 15:35:24 +0700

    p

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,5 +1,12 @@
 const comment = "TypeScript is a strongly typed programming language"
+const comm = comment.split("");
 
 function shortenComment(comment: string): string {
-  // Write your code below
+  let filtered: string[] = comm.filter((comm) => comm.length >= 5 && comm.length <= 10);
+
+  return filtered.toString();
 }
+
+shortenComment("TypeScript is a strongly typed programming language");
+
+//*** 
