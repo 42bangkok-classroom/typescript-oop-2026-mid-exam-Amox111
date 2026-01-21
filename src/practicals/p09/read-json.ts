f2267c2a4b6f9e85c938763f77be401b88ecae7a
Chomchanok khwammana <ruxkwarmmana233@gmail.com>
2026-01-21 16:19:46 +0700

    i

--- src/practicals/p09/read-json.ts
+++ src/practicals/p09/read-json.ts
@@ -1,3 +1,15 @@
+import fs from 'fs';    
+const filePath: string = 'user.json';
+
+
+
 function readJson(fileName: string): unknown[] {
-  
+    fs.readFile(filePath, 'utf8', (err, jsonString: string) => {
+        if (err) {
+          console.error('Invalid file', err);
+          return;
+        }
+      });
+
+      return [];
 }
